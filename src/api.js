223645379c4ccae8e2da3474a52e2226bc7a7826
Rich <applerich0306@gmail.com>
2022-10-31 23:04:19 +0200

    first commit

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const options = {
+export const geoApiOptions = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'b3a3f4b611msh94fa3d116beb296p180df0jsnb5738dabf27b',
@@ -6,7 +6,12 @@ export const options = {
 	}
 };
 
-export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions'
+// export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions'
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = '5cc7690a886767b4a2adb9b766603186';
 
 
 // fetch(GEO_API_URL, options)
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { AsyncPaginate } from 'react-select-async-paginate'
 
-import { GEO_API_URL, options} from '../../api';
+import { GEO_API_URL, geoApiOptions} from '../../api';
 
 const Search = ({onSearchChange}) =>
 {
@@ -9,17 +9,16 @@ const Search = ({onSearchChange}) =>
 
     const loadOptions = (inputValue) =>
     {
-        return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, options)
+        return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
         .then((response) => response.json())
             .then((response) =>
             {
                 return {
-                    options: response.data.map((city) =>
-                    { 
+                    options: response.data.map((city) => {
                         return {
-                            value: `${city.latitude} ${city.longtitude}`,
-                            label: `${city.name} ${city.countryCode}`
-                        }
+                        value: `${city.latitude} ${city.longitude}`,
+                        label: `${city.name}, ${city.countryCode}`,
+                        };
                     })
                 }
             }
